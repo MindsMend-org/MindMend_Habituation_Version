b24601475c748f884723f64608dce76be664df98
Brett Palmer <59998931+MindsMend-org@users.noreply.github.com>
2020-02-24 22:21:36 +0000

    Update MMHverAlpha.ino
    Bugs fixed tested runs close enough to target of 40hz, till I buy a decent oscilloscope. Then I will adjust to  perfection and update.
    Mince

--- MMHverAlpha.ino
+++ MMHverAlpha.ino
@@ -10,8 +10,8 @@
 
 
 //Based on 8 Coil Setup (Timings)-
-//Primer 25ms = 8 Coil Setup.       (Primer 360 seconds)
-//Binding = 3.125ms = 8 Coil Setup. (Effector 840 seconds)
+//Primer 25ms = 8 Coil Setup.       (Primer 360 seconds)-set to 36 seconds atm ten loops = 1 use
+//Binding = 3.125ms = 8 Coil Setup. (Effector 840 seconds) - set to 420 atm = 7min
 
 //The Visual Cortex Is located at Back of Head.
 //     Left eye = Right Hemisphere (B)
@@ -23,38 +23,43 @@
 unsigned long Time = 0;
 //const float Runtime = 1000*60*60; //ms/sec/min = 1 hour. 
 
-//Lights Can be uv 740nm> 1000nm< if want to be Invisable to eye
-// as Visual Cortex has been shown to react to light wavelenghts
-// beyond standard visual spectrum range at thease freaquencies. Has Implications of making invisible light lamps.
 
 //This Device Will Use Standard Vis spec Band. (450nmBlue or SuperBrightWhite LED) 3.5v 50mA use at least a 39R resistor.(Works Well with a 150R)R = ohm.  
 //EYE LED 
 //ATMega2560               01,02,03,04,05,06,07,08,09,10 //index = 0-9
-int LeftEyePinArray[10] = {24,26,28,30,32,34,36,38,40,42};//Critical to keep these 2 arrays the same length.
-int RightEyePinArray[10] ={23,25,27,29,31,33,35,37,39,41};
+int LeftEyePinArray[] = {23,25,27,29,31,33,35,37,39,41,23,25,27,29,31,33,35,37,39,41,23,25,27,29,31,33,35,37,39,41,23,25,27,29,31,33,35,37,39,41,25,31,37,23,27,33,39,29,35,41,25,31,37,23,27,33,39,29,35,41,25,31,37,23,27,33,39,29,35,41};//Critical to keep these 2 arrays the same length.
+int RightEyePinArray[] ={22,24,26,28,30,32,34,36,38,40,22,24,26,28,30,32,34,36,38,40,22,24,26,28,30,32,34,36,38,40,22,24,26,28,30,32,34,36,38,40,24,30,36,22,26,32,38,28,34,40,22,24,26,28,30,32,34,36,38,40,24,30,36,22,26,32,38,28,34,40};
+//Led div 2 offset 
+//r= ={22,24,26,28,30,32,34,36,38,40,22,24,26,28,30,32,34,36,38,40,22,24,26,28,30,32,34,36,38,40,22,24,26,28,30,32,34,36,38,40,22,24,26,28,30,32,34,36,38,40};
+//l= = {23,25,27,29,31,33,35,37,39,41,23,25,27,29,31,33,35,37,39,41,23,25,27,29,31,33,35,37,39,41,23,25,27,29,31,33,35,37,39,41,23,25,27,29,31,33,35,37,39,41};
+int OFFSETLeftEyePinArray[] = {23,25,27,29,31,33,35,37,39,41,23,25,27,29,31,33,35,37,39,41,23,25,27,29,31,33,35,37,39,41,23,25,27,29,31,33,35,37,39,41,25,31,37,23,27,33,39,29,35,41,25,31,37,23,27,33,39,29,35,41,25,31,37,23,27,33,39,29,35,41};
+int OFFSETRightEyePinArray[] ={22,24,26,28,30,32,34,36,38,40,22,24,26,28,30,32,34,36,38,40,22,24,26,28,30,32,34,36,38,40,22,24,26,28,30,32,34,36,38,40,24,30,36,22,26,32,38,28,34,40,22,24,26,28,30,32,34,36,38,40,24,30,36,22,26,32,38,28,34,40};
+
 int EYEINDEX = 0;//LED_BUILTIN;//test using board led
 int RLight = RightEyePinArray[EYEINDEX];
 int LLight = LeftEyePinArray[EYEINDEX];
+
+//OFFSET
+int OFFSETEYEINDEX = 0;//LED_BUILTIN;//test using board led
+int OFFSETRLight = RightEyePinArray[EYEINDEX];
+int OFFSETLLight = LeftEyePinArray[EYEINDEX];
 
 int ArraySIZE = sizeof(LeftEyePinArray)/sizeof(RightEyePinArray[0]);//get size of array in bits & convert to index elements. Will b value of 9 as (0-9 = 10)
 
 //Eye Pins Clarification. so Fires R-Eye Coil L-Eye eg pin order 4,LEye,5,REye,6 !!! Anit-Clockwise around head, Fire in PinID sequence
 
 
-//Coil Array Refrence
-//int PinArray[] = {1,2,3,4,5,6,7,8};
-//int PinRaiseTime[10] = {0,25,50,75,100,125,150,175,25+-Hoffset,25+-Hoffset};
 
 
 //Coils Direct Refrence>Pin Address
-int CoilA = 1; //1
-int CoilB = 2; //2
-int CoilC = 3; //3
-int CoilD = 4; //4
-int CoilE = 5; //5
-int CoilF = 6; //6
-int CoilG = 7; //7
-int CoilH = 8; //8
+int CoilA = 2; //1
+int CoilB = 3; //2
+int CoilC = 4; //3
+int CoilD = 5; //4
+int CoilE = 6; //5
+int CoilF = 7; //6
+int CoilG = 8; //7
+int CoilH = 9; //8
 
 
 
@@ -75,15 +80,15 @@ pinMode(CoilH, OUTPUT);
 
 
 //Pin Mode: Set to Driver Mode for Mosfet Operation.(SETUP-LOOP)
-EYEINDEX=-0;//-1?--------------------------------------------------------------------
-for (int i = 0;i <= 9;i++){
-EYEINDEX +=1;
+for (int i = 0;i < ArraySIZE;i++){
+EYEINDEX = i; //+=1;
 RLight = RightEyePinArray[EYEINDEX];
 LLight = LeftEyePinArray[EYEINDEX];
 pinMode(LLight, OUTPUT);
 pinMode(RLight, OUTPUT);
-digitalWrite(RLight, LOW);
 digitalWrite(LLight, LOW);
+digitalWrite(RLight, LOW);
+
 }
 //Pin Start Set All Outputs to off
 //Eyes
@@ -101,20 +106,39 @@ digitalWrite(CoilG, LOW);
 digitalWrite(CoilH, LOW);
 }
 
+
+
+
+
+
+
+
 void loop() {
 //Loop through pins.
 //P R I M E R
 //360 second Loop = 180ms once through time. 5.555555555555556hz
 //Coils
-EYEINDEX = 0;// to start.
-int timeforloop = (1000/180) * 10;// 360; //=1sec/oncethroughtime*360 seconds = 2000 loops
+EYEINDEX = -1;// to start.
+OFFSETEYEINDEX = 5;
+
+int timeforloop = (1000/180) * 36;// 360; //=1sec/oncethroughtime*360 seconds = 2000 loops
+
 for (int i = 0; i <= timeforloop ; i++) {
 EYEINDEX +=1;
-if (EYEINDEX == ArraySIZE){
+OFFSETEYEINDEX +=1;
+
+if (EYEINDEX == ArraySIZE-1){
   EYEINDEX = 0;
+  }
+if (OFFSETEYEINDEX == ArraySIZE-1){
+  OFFSETEYEINDEX =0;
   }
 RLight = RightEyePinArray[EYEINDEX];
 LLight = LeftEyePinArray[EYEINDEX];
+//Offset
+OFFSETRLight = OFFSETRightEyePinArray[OFFSETEYEINDEX];
+OFFSETLLight = OFFSETLeftEyePinArray[OFFSETEYEINDEX];
+
 digitalWrite(CoilA, HIGH); //1 on A       
 delay(2.5);
 digitalWrite(CoilH, LOW);  //8 off
@@ -129,15 +153,19 @@ digitalWrite(CoilB, LOW);  //2 off
 delay(20.0);
 digitalWrite(CoilD, HIGH); //4 on D
 digitalWrite(RLight, HIGH);//RightEyePinArray[EYEINDEX]
+digitalWrite(OFFSETRLight, HIGH);
 delay(2.5);
 digitalWrite(LLight, HIGH);//RightEyePinArray[EYEINDEX]
+digitalWrite(OFFSETLLight, HIGH);
 delay(2.5);
 digitalWrite(CoilC, LOW);  //3 off
 delay(17.5);
 digitalWrite(RLight, LOW);
+digitalWrite(OFFSETRLight, LOW);
 digitalWrite(CoilE, HIGH); //5 on E
 delay(2.5);
 digitalWrite(LLight, LOW);
+digitalWrite(OFFSETLLight, LOW);
 digitalWrite(CoilD, LOW);  //4 off
 delay(20.0);
 digitalWrite(CoilF, HIGH); //6 on F
@@ -158,15 +186,26 @@ delay(20.0);
 //B I N D I N G
 //Binding = 3.125ms = 8 Coil Setup. (Effector 840 seconds) 40hz
 //840 Seconds Binding Mode
-EYEINDEX = 0;//to start.
-timeforloop = 40 * 210;//840; // = 1 sec / once through = 25ms * looptime seconds
-for (int Binding = 0;Binding <= timeforloop;Binding ++){
+EYEINDEX = -1;//to start.
+OFFSETEYEINDEX = 5;
+timeforloop = 40 * 420;//840; // = 1 sec / once through = 25ms * looptime seconds
+for (int Binding = 0;Binding < timeforloop;Binding ++){
 EYEINDEX +=1;
-if (EYEINDEX == ArraySIZE){
+OFFSETEYEINDEX +=1;
+
+if (EYEINDEX == ArraySIZE-1){
   EYEINDEX = 0;
+  }
+  if (OFFSETEYEINDEX == ArraySIZE-1){
+  OFFSETEYEINDEX = 0;
   }
 RLight = RightEyePinArray[EYEINDEX];
 LLight = LeftEyePinArray [EYEINDEX];
+//Offset
+OFFSETRLight = OFFSETRightEyePinArray[OFFSETEYEINDEX];
+OFFSETLLight = OFFSETLeftEyePinArray[OFFSETEYEINDEX];
+
+
 digitalWrite(CoilA, HIGH); //1 on A       
 delay(0.125);
 digitalWrite(CoilH, LOW);  //8 off
@@ -181,14 +220,18 @@ digitalWrite(CoilB, LOW);  //2 off
 delay(3);
 digitalWrite(CoilD, HIGH); //4 on D
 digitalWrite(RLight, HIGH);         //Right Eye Drive Eye Array
+digitalWrite(OFFSETRLight, HIGH); 
 delay(0.125);
 digitalWrite(LLight,HIGH);          //Left Eye Drive Eye Array
+digitalWrite(OFFSETLLight,HIGH);
 digitalWrite(CoilC, LOW);  //3 off
 delay(3);
 digitalWrite(RLight, LOW);          //Right Eye Turn Off Current Eye Index
+digitalWrite(OFFSETRLight, LOW);
 digitalWrite(CoilE, HIGH); //5 on E
 delay(0.125);
 digitalWrite(LLight, LOW);          //Left Eye Tuen Off Current Eye Index
+digitalWrite(OFFSETLLight, LOW);
 digitalWrite(CoilD, LOW);  //4 off
 delay(3);
 digitalWrite(CoilF, HIGH); //6 on F
